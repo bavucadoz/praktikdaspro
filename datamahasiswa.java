0256b40546fc51c060e4dfd9b1c2d00258186505
bavucadoz <aryabayusamodra@gmail.com>
2024-09-10 13:59:09 +0700

    isi biodata

---
+++ datamahasiswa.java
@@ -0,0 +1,14 @@
+public class datamahasiswa {
+
+    public static void main(String[] args) {
+        System.out.println("Data Mahasiswa");
+        System.out.println("nama \t:");
+        System.out.println("nim \t:");
+        System.out.println("kelas \t:");
+        System.out.println("no absen:");
+        System.out.println("prodi \t:");
+        System.out.println("alamat \t:");
+        System.out.println("no hp \t:");
+        System.out.println("hobi \t:");
+    }
+}
