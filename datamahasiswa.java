65f142809caff558bca223d66aeef2c69a9ca88c
joenasiq <arjunash2006@gmail.com>
2024-09-10 17:20:23 +0700

    don bg

--- datamahasiswa.java
+++ datamahasiswa.java
@@ -2,13 +2,13 @@ public class datamahasiswa {
 
     public static void main(String[] args) {
         System.out.println("Data Mahasiswa");
-        System.out.println("nama \t:");
-        System.out.println("nim \t:");
-        System.out.println("kelas \t:");
-        System.out.println("no absen:");
-        System.out.println("prodi \t:");
-        System.out.println("alamat \t:");
-        System.out.println("no hp \t:");
-        System.out.println("hobi \t:");
+        System.out.println("nama \t: Arjuna Satria Hutama  ");
+        System.out.println("nim \t: 244107020158");
+        System.out.println("kelas \t: 1A");
+        System.out.println("no absen: 06");
+        System.out.println("prodi \t: D-IV Teknologi Informasi");
+        System.out.println("alamat \t: Jl.Bylira No. 164");
+        System.out.println("no hp \t: 089530526247");
+        System.out.println("hobi \t: Mendengarkan Musik");
     }
 }
